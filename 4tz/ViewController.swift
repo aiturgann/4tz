b520a09beded5b23dededeb4e1645f56fbd13d3b
aiturann <aiturgannnnnn@gmail.com>
2024-02-25 22:37:56 +0600

    Initial Commit

---
+++ 4tz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4tz
+//
+//  Created by Aiturgan Kurmanbekova on 25/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
